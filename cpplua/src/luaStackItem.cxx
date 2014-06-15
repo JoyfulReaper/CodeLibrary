8a560a03dc6df32c690bcb26b126ad874ef3e8b5
Kyle Givler <xvegan88x@gmail.com>
2014-06-15 19:29:54 -0400

    C++ IS KILLING ME TODAY

---
+++ cpplua/src/luaStackItem.cxx
@@ -0,0 +1,30 @@
+/*
+    cppLua: luaStackItem.cxx
+    Copyright (C) 2014 Kyle Givler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/**
+* @file luaStackItem.cxx
+* @author Kyle Givler
+*/
+
+#include "luaStackItem.hpp"
+#include "luaExcept.hpp"
+
+namespace cppLua
+{
+}
