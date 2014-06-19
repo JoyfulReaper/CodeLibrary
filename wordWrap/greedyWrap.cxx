db7cd0f891327a69579b35615df1e12188d70157
Kyle Givler <xvegan88x@gmail.com>
2014-06-18 20:14:50 -0400

    Working word wrap class

--- wordWrap/greedyWrap.cxx
+++ wordWrap/greedyWrap.cxx
@@ -1,7 +1,32 @@
+/*
+    GreedyWrap: greedyWrap.cxx
+    Copyright (C) 2014 Kyle Givler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/**
+ * @file GreedyWrap.cxx
+ * @author Kyle Givler
+ * 
+ * Greedy word wrapping class
+ */
+
 #include "greedyWrap.hpp"
 
 // TODO: Probably need to strip tabs (/t) maybe others
-// FIXME: The last word is not processed
 
 static const size_t SPACE_WIDTH = 1;
 
@@ -27,8 +52,14 @@ std::string GreedyWrap::lineWrap(std::stringstream &ss)
     }
   }
   
-  
   return output;
+}
+
+std::string GreedyWrap::lineWrap(std::string string)
+{
+  std::stringstream ss;
+  ss.str(string);
+  return lineWrap(ss);
 }
 
 bool GreedyWrap::getWord(std::stringstream &ss, std::string &outWord)
@@ -39,7 +70,7 @@ bool GreedyWrap::getWord(std::stringstream &ss, std::string &outWord)
   {
     letter = ss.get();
     
-    if (letter == ' ')
+    if (letter == ' ' || !ss.good())
     {
       outWord = word;
       return true;
--- wordWrap/test.cpp
+++ wordWrap/test.cpp
@@ -4,8 +4,10 @@
 int main (void)
 {
   std::stringstream ss;
+  std::string test = "test string";
   
   ss << "TEST THIS is just a test! YOU can ignore this test! THIS IS JUST A BUNCH OF SHIT NOW SO IGNORE THIS SHIT OK? ksdhfa lsdkjfh laskdjhflk asdhkjfdh bavxcvn  akdlhgakdsjhgk lsjdhflkjsdha lfkjhdsakg. hkdls j;lkfhr luesafkhdkgh lakjsdhfdkjlsgfkjasfdh gkjsda ;lklhglkjdsfh gkj;dh fsklajhgj kldgajlk dhlkjghkj hsdf;gj al;ksdjglkfdsjf la;sjdlgk hjh fslkd;ajf;lkjdgkl aj;sdlkgj ;lkdsajgl;kj kjds; falgh kkjdhsdkjgfds ggfa. This is less shitty, i just need to think of words to write to test this shitty algoritm with. This wasn't too hard to write, so I don't know why all of the implementations that I found online suck so damn hard! one moer word.";
+  //ss << "For the debugger!";
   
   GreedyWrap wrap;
   std::cout << wrap.lineWrap(ss);
